9b8ed83e70546d8ea8a6b3d30132f913c1f707ab
GalElf <gal.elefersy@gmail.com>
2021-03-15 16:27:29 +0200

    build app first design

--- app/src/main/java/com/gale_matany/ex1/GameActivity.java
+++ app/src/main/java/com/gale_matany/ex1/GameActivity.java
@@ -3,12 +3,60 @@ package com.gale_matany.ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private int mm;
+    private int ss;
+    private String seconds;
+    private String minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        TextView gameTime = findViewById(R.id.game_time);
+        countTime(gameTime);
+
+
+    }
+    
+
+    private void countTime(TextView gameTime){
+        mm = 0;
+        ss = 0;
+        seconds = "00";
+        minutes = "00";
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    ss++;
+                    if(ss == 60){
+                        mm++;
+                        ss = 0;
+                    }
+                    seconds = "" + ss;
+                    minutes = "" + mm;
+                    if(ss < 10){
+                        seconds = "0" + ss;
+                    }
+                    if(mm < 10){
+                        minutes = "0" + mm;
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            gameTime.setText("Time: " + minutes + ":" + seconds);
+                        }
+                    });
+                    SystemClock.sleep(1000);
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/com/gale_matany/ex1/MainActivity.java
+++ app/src/main/java/com/gale_matany/ex1/MainActivity.java
@@ -1,14 +1,112 @@
 package com.gale_matany.ex1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playBtn = findViewById(R.id.play_btn_id);
+        playBtn.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(R.id.play_btn_id == v.getId()){
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+        // menu options shown in the 3 dots menu
+        MenuItem aboutMenu = menu.add("About");
+        MenuItem exitMenu = menu.add("Exit");
+        // for the about app
+        aboutMenu.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener()
+        {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                showAboutDialog();
+                return true;
+            }
+        });
+        // for the exit app
+        exitMenu.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener()
+        {
+            @Override
+            public boolean onMenuItemClick(MenuItem item)
+            {
+                showExitDialog();
+                return true;
+            }
+        });
+        return true;
+    }
+
+    public void showAboutDialog()
+    {
+        String aboutApp = getString(R.string.app_name) +" (" + getPackageName() + ")\n\n" +
+                "By "+ getString(R.string.creators) + ", 24/03/2021.";
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setIcon(R.mipmap.ic_launcher);
+        dialog.setTitle(getString(R.string.about_app_title));
+        dialog.setMessage(aboutApp);
+        dialog.setCancelable(false);
+        dialog.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public void showExitDialog()
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setIcon(R.drawable.ic_exit);
+        dialog.setTitle(getString(R.string.exit_app_title));
+        dialog.setMessage(getString(R.string.exit_app));
+        dialog.setCancelable(false);
+        dialog.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                // close the app
+                finish();
+            }
+        });
+        dialog.setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {}
+        });
+        dialog.show();
+    }
+
 }
